b17022373d5620a01d7b4daf42169aa8f7aded02
jeweler <valve.valve101@gmail.com>
2024-07-18 13:52:00 -0400

    simple ping command

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
-print("hello world!")
+import os
+import discord
+# import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+class DiscordClient(discord.Client):
+    async def on_read(self):
+        print(f"Logged in as {self.user}!")
+
+    async def on_message(self, message):
+        print(f"Message from {message.author}: {message.content}")
+        match message.content:
+            case '!ping':
+                await message.reply("pong")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = DiscordClient(intents=intents)
+client.run(TOKEN)
